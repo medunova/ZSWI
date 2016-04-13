b528f7d5ac4448698ca7e12492b74c5578374c85
medunova <medunova.anet@seznam.cz>
2016-04-13 12:48:26 +0200

    Poupravena tabulka okno exportuj a hlavicka v pdf

---
+++ Generovani.java
@@ -0,0 +1,213 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.draw.VerticalPositionMark;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+/**
+ * Trida pro vygenerovani testu
+ * @author Aneta Medunova, Tomas Zobac, Michal Vselko
+ *
+ */
+public class Generovani {
+
+	ArrayList<Otazka> vybraneOtazky = new ArrayList<Otazka>();								//seznam vygenerovaných otázek
+	ArrayList<Otazka> nevybraneOtazky;														//seznam nevybraných otázek
+
+    /** Cesta a nazev vytvoreneho PDF souboru */
+    public static String pdfNazev;
+
+    /**
+     * Konstruktor tøídy pro generování a export do pDF
+     * @param otazky seznam všech otázek
+     * @param skola název školy
+     * @param zkous název zkoušejícího
+     * @param body	true, pokud byl vybrán export podle poètu bodù
+     * @param pocet poèet bodù nebo otázek
+     * @param idTest id testu
+     * @param datum datum zkoušky
+     */
+	public Generovani(ArrayList<Otazka> otazky, String idTest, String datum, String skola, String zkous, String predmet, Boolean body, int pocet) {
+
+		this.pdfNazev = "./"+idTest+".pdf";
+		try {
+			nevybraneOtazky = (ArrayList<Otazka>)otazky.clone();
+			generujOtazky(nevybraneOtazky, body, pocet);
+			vytvorPDF(skola, zkous,predmet, datum, vybraneOtazky);
+
+		}
+		catch (DocumentException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Export dat.");
+			alert.setHeaderText("Chyba pøi exportu dat do PDF!");
+			alert.setContentText("Nepodaøilo se vyexportovat data do PDF.");
+			alert.show();
+		}
+		catch (Exception e){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Export dat.");
+			alert.setHeaderText("Chyba pøi exportu dat do PDF!");
+			alert.setContentText("Nepodaøilo se vyexportovat data do PDF.");
+			alert.show();
+		}
+
+	}
+
+	/**
+	 * Metoda pro generování náhodných otázek
+	 * @param otazky celkový seznam otázek pro náhodný výbìr
+	 * @param body true pokud se test generuje podle poètu bodù, jinak false
+	 * @param pocet poèet bodù nebo otázek
+	 */
+	private void generujOtazky(ArrayList<Otazka> otazky, Boolean body, int pocet){
+		Random rnd = new Random();
+		if(body) {																			//je-li true, bylo vybráno generování podle poètu bodù
+			int aktualniPocet = 0;
+			int pocetOpakovani = 0;
+			while(aktualniPocet != pocet){													//cyklus bude probíhat dokuk poèet bodù náhodnì vygenerovaných otázek nebude požadovaný poèet
+				int nahodneCislo = rnd.nextInt(nevybraneOtazky.size());						//vygeneruje se náhodné èíslo (max je poèet nevybraných otázek)
+				Otazka otazka = nevybraneOtazky.get(nahodneCislo);							//ze seznamu nevybraných otázek se vybere otázka pod náhodnì vygenerovaným indexem
+				if((aktualniPocet + otazka.getPocetBodu()) <= pocet){						//aktuální poèet bodù se seète s poètem bodù nové otázky a porovná se cílovým poètem bodù
+					vybraneOtazky.add(otazka);												//pokud poèet bodù nepøesáhne cílový poèet, pøidá se otázka do vybraných, odebere se z nevybraných a zaktualizuje se aktuální poèet bodù
+					nevybraneOtazky.remove(otazka);
+					aktualniPocet += otazka.getPocetBodu();
+				}
+				pocetOpakovani++;															/*poèet opakování se musí hlídat z dùvodu možného zacyklení
+																							  bude-li napø. poèet aktuálních bodù 49 a už nebude žádná otázka s 1 bodem pro cílových 50 bodù
+																							  pobìží cyklus poøád dokola - pøi 150 opakování se vybrané otázky vymažou a do nevybraných se doplní
+																							  všechny otázky a náhodné generování bude probíhat od zaètku
+																							*/
+				if(pocetOpakovani == 150){
+					vybraneOtazky.clear();
+					nevybraneOtazky = otazky;
+					aktualniPocet = 0;
+					pocetOpakovani = 0;
+				}
+			}
+		}
+		else{
+			for(int i = 0; i < pocet; i++){													//generování otázek podle nastaveného poètu otázek
+				int nahodneCislo = rnd.nextInt(nevybraneOtazky.size());
+				Otazka otazka = nevybraneOtazky.get(nahodneCislo);
+				vybraneOtazky.add(otazka);
+				nevybraneOtazky.remove(otazka);
+			}
+		}
+	}
+
+	private void vytvorPDF(String skola, String zkousejici,String predmet, String datum, ArrayList<Otazka> vybraneOtazky) throws DocumentException, IOException{
+
+
+    	// Nastaveni velikosti dokumentu
+        Rectangle velikostStranky = new Rectangle(PageSize.A4);	//nastavi rozmery dokumentu A4
+        Document document = new Document(velikostStranky, 30f, 30f, 20f, 15f);	//vytvoreni dokumentu o velikosti velikostStranky a nastaveni okraju
+
+        PdfWriter.getInstance(document, new FileOutputStream(pdfNazev));	//vytvoreni PDF
+
+        document.open();	//otevre souboru
+
+        //Pridani ceskeho fontu
+        BaseFont bfArial = BaseFont.createFont
+        		(
+        		"c:\\windows\\fonts\\arial.ttf",
+        		BaseFont.IDENTITY_H,
+        		BaseFont.EMBEDDED
+        		);
+
+
+
+        document.addTitle("Vygenerovany test");	//nastavi titulek dokumentu
+
+        Font hlavickaFont = new Font(bfArial, 14.0f, Font.NORMAL, BaseColor.BLACK); //nastaveni pisma hlavicky
+
+        //Vypise hlavicku
+        Chunk rozmisteni = new Chunk(new VerticalPositionMark());
+        Paragraph hlavicka = new Paragraph(skola,hlavickaFont);
+        hlavicka.add(new Chunk(rozmisteni));
+        hlavicka.add("Pøíjmení a jméno: ....................");
+        document.add(hlavicka);
+        hlavicka.clear();
+
+        hlavicka.add(predmet);
+        hlavicka.add(new Chunk(rozmisteni));
+        hlavicka.add("Osobní èíslo: ....................");
+        document.add(hlavicka);
+        hlavicka.clear();
+
+
+        hlavicka.add("Zkoušející: "+zkousejici);
+        hlavicka.add(new Chunk(rozmisteni));
+        hlavicka.add("Tým: ....................");
+        document.add(hlavicka);	//zapise do souboru
+
+        hlavicka.clear();
+        hlavicka.add("Datum: "+datum);
+        document.add(hlavicka);	//zapise do souboru
+        document.add(new Paragraph(" ")); //odradkuje
+
+        Font otazkyFont = new Font(bfArial, 11.0f, Font.NORMAL, BaseColor.BLACK); //nastaveni pisma otazek
+
+        tiskOtazek(document, otazkyFont);
+        document.close();	//zavre soubor
+	}
+
+	/**
+	 * Metoda seøadí a pøidá do exportu jednotlivé otázky
+	 * @param document soubor který se exportuje
+	 * @param otazkyFont font otázek
+	 */
+	private void tiskOtazek(Document document, Font otazkyFont) throws DocumentException{
+		int prostor = 40;																							//poèet øádkù na první stránce pro otázky
+		boolean nalezeno = false;																					//nalezena otázka pro vložení na stránce
+		int cislo = 0;
+		Comparator comp = Comparator.comparing(Otazka::getMisto);
+		Collections.sort(vybraneOtazky, Collections.reverseOrder(comp));											//seøadí otázky v seznamu od nejvìtší velikosti místa
+
+		while(vybraneOtazky.size() > 0){																			//cyklus pobìží dokud nejsou na stránce všechny vygenerované otázky
+			do{
+				nalezeno = false;
+				for(int i = 0; i < vybraneOtazky.size(); i++){														//cyklus projde všechny otázky dokud nenarazí na tu která se na stránku vejde
+					Otazka otazka = vybraneOtazky.get(i);
+					int pocetRadku = otazka.getPocetRadkuText() + otazka.getPocetRadkuMisto();						//nastaví se poèet nutných øádkù pro otázku
+					if((prostor - pocetRadku) >= 0){																//vejde-li se otázka do zbývajícího prostoru, doplní se do dokumentu
+						Paragraph otazkaTisk = new Paragraph(++cislo + "." + otazka.getOtazka(), otazkyFont);
+						document.add(otazkaTisk);
+						prostor -= otazka.getPocetRadkuText();
+						for(int j = 0; j < otazka.getPocetRadkuMisto(); j++)										//nastaví volné øádky pro odpovìï
+					        if(--prostor > 0) document.add(new Paragraph(" "));
+
+						vybraneOtazky.remove(otazka);																//otázka se odebere ze seznamu
+						nalezeno = true;
+						break;
+					}
+				}
+			}
+			while (nalezeno);																						/*pokud nebyla otázka nalezena, znamená to buï že je seznam prázdný
+																													  nebo se na stránku už žádná z vygenerovaných otázek nevejde
+																													 */
+			if(vybraneOtazky.size() > 0)																			//pokud jsou stále nìjaké vygenerované otázky, vytvoøí se nová stránka a cyklus zaène znovu
+				document.newPage();
+
+			prostor = 43;																							//nová velikost prostoru pro stránky bez hlavièky
+		}
+	}
+
+
+
+}
---
+++ oknoExportuj.java
@@ -0,0 +1,181 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.*;
+
+public class oknoExportuj extends Stage {
+
+	final ToggleGroup group = new ToggleGroup();
+	private BorderPane hlPanel;
+	private ToggleButton PBody, POtazky;
+	private TextField pocty, institut, zkousejici, datum, predmet;
+	private Button tisk;
+	private ArrayList<Otazka> otazky;
+	private ArrayList<Kategorie> kategorie;
+	private static Scanner sc;
+	private FileWriter writer;
+	private Label done;
+
+	public oknoExportuj(ArrayList<Kategorie> kategorie, ArrayList<Otazka> otazky) {
+		this.kategorie = kategorie;
+		this.otazky = otazky;
+
+		hlPanel = new BorderPane();
+
+		hlPanel.setTop(hlavicka());
+		hlPanel.setCenter(vyber());
+		hlPanel.setBottom(tlacitka());
+
+		Scene scene = new Scene(hlPanel, 600, 400);
+		this.setScene(scene);
+		scene.getStylesheets().add
+		 (oknoExportuj.class.getResource("oknoExportuj.css").toExternalForm());
+
+		this.setTitle("Exportovat do pdf");
+	}
+
+	private VBox hlavicka() {
+		VBox vbox = new VBox(15);
+		vbox.setPadding(new Insets(20));
+		vbox.setAlignment(Pos.CENTER);
+		HBox radek1 = new HBox(10);
+		HBox radek2 = new HBox(10);
+		HBox radek3 = new HBox(10);
+
+		institut = new TextField();
+		institut.setText("Fakulta aplikovaných vìd");
+		Label text1 = new Label("Škola: ");
+		text1.setId("text1");
+		zkousejici = new TextField();
+		zkousejici.setText("Roman Mouèek");
+		Label text2 = new Label("Zkoušející: ");
+		text2.setId("text2");
+		predmet = new TextField();
+		predmet.setText("Základy softwarového inženýrství");
+		Label text5 = new Label("Pøedmìt: ");
+		text5.setId("text5");
+
+		String idTestu = idTestu();
+		Label text3 = new Label("ID testu: " + '\t' + idTestu);
+		text3.setId("text3");
+		datum = new TextField();
+		Label text4 = new Label('\t' + "Datum testu: ");
+		text4.setId("text4");
+
+
+		radek1.getChildren().addAll(text1,institut,text2,zkousejici);
+		radek2.getChildren().addAll(text5,predmet,text4, datum);
+		radek3.getChildren().addAll(text3);
+
+		vbox.getChildren().addAll(radek1, radek2,radek3);
+
+		return vbox;
+	}
+
+	private VBox vyber() {
+		VBox vbox = new VBox(10);
+		vbox.setAlignment(Pos.CENTER);
+		vbox.setPadding(new Insets(15));
+
+		pocty = new TextField();
+		Label popis = new Label("Zadejte pocet: ");
+		popis.setId("popis");
+
+		PBody = new ToggleButton("Poèet bodù");
+		PBody.setToggleGroup(group);
+		PBody.setSelected(true);
+		PBody.setId("PBody");
+
+		POtazky = new ToggleButton("Poèet otázek");
+		POtazky.setToggleGroup(group);
+		POtazky.setId("POtazky");
+
+		Label text = new Label("Vybrat náhodnì dle: ");
+		text.setId("text");
+
+		HBox radek1 = new HBox(10);
+		radek1.getChildren().addAll(text, PBody, POtazky);
+		radek1.setAlignment(Pos.CENTER);
+
+		HBox radek2 = new HBox(50);
+		radek2.getChildren().addAll(popis, pocty);
+		radek2.setAlignment(Pos.CENTER);
+
+		vbox.getChildren().addAll(radek1, radek2);
+		return vbox;
+	}
+
+	private VBox tlacitka() {
+		VBox hbox = new VBox(5);
+		hbox.setAlignment(Pos.CENTER);
+		hbox.setPadding(new Insets(5,5,20,5));
+		tisk = vytiskni();
+		tisk.setId("tisk");
+		done = new Label("");
+
+		hbox.getChildren().addAll(tisk, done);
+
+		return hbox;
+	}
+
+	private Button vytiskni() {
+		Button bt = new Button("Vytvoø test");
+		bt.setOnAction((ActionEvent e) -> {
+			if(PBody.isSelected() == true) {
+				try {
+					new Generovani(otazky, idTestu(), datum.getText(), institut.getText(), zkousejici.getText(), predmet.getText(), true, Integer.parseInt(pocty.getText()));
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+
+			if(POtazky.isSelected() == true) {
+
+				System.out.println("Test byl vytvoøen");
+				try {
+					new Generovani(otazky, idTestu(), datum.getText(), institut.getText(), zkousejici.getText(), predmet.getText(), false, Integer.parseInt(pocty.getText()));
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			done.setText(" Test byl vytvoren");
+		});
+
+		return bt;
+	}
+
+	private String idTestu() {
+		String s;
+		int p = 0;
+		try {
+			sc = new Scanner(new File("IDTESTU.txt"));
+			s = sc.next();
+			p = Integer.parseInt(s) + 1;
+
+			writer = new FileWriter("IDTESTU.txt");
+			writer.write(Integer.toString(p));
+			writer.close();
+		}
+		catch (Exception e) {
+			System.out.println("Soubor nenalezen");
+		}
+		s = "ZSWI2016" + String.format("%03d", p);
+		return s;
+	}
+}
